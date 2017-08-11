62c764fb31a04d813ef74205f880af396a0f94e9
hueidou <hueidou163@gmail.com>
2017-08-11 13:48:24 +0800

    modify & delete manday

--- server/controllers/Manday.js
+++ server/controllers/Manday.js
@@ -5,9 +5,15 @@ var db = require('../db/db.js');
  * 获取工时列表
  */
 module.exports.getMandays = function (req, res, next) {
-  var params = req.swagger.params;
+  var show = req.swagger.params["show"].value;
+  var userName = req.headers["api_key"];
 
-  db.mandays.find({})
+  var filter = {};
+  if (show == "me") {
+    filter["userName"] = userName;
+  }
+
+  db.mandays.find(filter)
     .sort({ date: 'desc' })
     .exec(function (err, docs) {
       res.setHeader('Content-Type', 'application/json');
@@ -37,6 +43,52 @@ module.exports.addManday = function (req, res, next) {
       manday.save(function (err) {
         res.end();
       });
+    }
+  });
+};
+
+/**
+ * 编辑工时
+ */
+module.exports.modifyManday = function (req, res, next) {
+  var _id = req.swagger.params._id.value;
+  var model = req.swagger.params.body.value;
+  var userName = req.headers["api_key"];
+
+  db.mandays.findOne({ _id: _id, userName: userName }, function (err, doc) {
+    if (!!doc) {
+      //var manday = new db.mandays();
+      doc.date = model.date;
+      doc.projectKey = model.projectKey;
+      //doc.userName = userName;
+      doc.hours = model.hours;
+      doc.description = model.description;
+
+      db.mandays.update({ _id: _id }, doc, function (err) {
+        res.end();
+      });
+    }
+    else {
+      res.end();
+    }
+  });
+};
+
+/**
+ * 删除工时
+ */
+module.exports.deleteManday = function (req, res, next) {
+  var _id = req.swagger.params._id.value;
+  var userName = req.headers["api_key"];
+
+  db.mandays.findOne({ _id: _id, userName: userName }, function (err, doc) {
+    if (!!doc) {
+      db.mandays.remove({ _id: _id }, function (err) {
+        res.end();
+      });
+    }
+    else {
+      res.end();
     }
   });
 };
--- src/components/Manday.vue
+++ src/components/Manday.vue
@@ -3,7 +3,8 @@
     <el-card>
       <div slot="header" class="card-header">
         <span>工时</span>
-        <el-button type="primary" icon="plus" size="mini" style="float:right;" @click="openAddForm()"></el-button>
+        <el-button type="primary" icon="plus" size="mini" style="float:right;" @click="openMandayForm()"></el-button>
+        <el-switch v-model="show" on-text="我" off-text="全部" off-color="#13ce66" on-value="me" off-value="all" style="float: right; margin-right: 10px;"></el-switch>
       </div>
       <el-table :data="mandays" :border="true">
         <el-table-column type="expand">
@@ -12,30 +13,36 @@
               <el-form-item label="描述：">
                 <span>{{ props.row.description }}</span>
               </el-form-item>
+              <el-form-item style="text-align:right" v-if="props.row.userName == currentUser">
+                <el-button-group>
+                  <el-button size="small" icon="delete" @click="deleteManday(props.$index, props.row)"></el-button>
+                  <el-button size="small" icon="edit" @click="openMandayForm(props.row)"></el-button>
+                </el-button-group>
+              </el-form-item>
             </el-form>
           </template>
         </el-table-column>
-        <el-table-column prop="date" label="日期" width="120" :formatter="dateFormatter">
+        <el-table-column prop="date" label="日期" width="120px" :formatter="dateFormatter">
         </el-table-column>
-        <el-table-column prop="userName" label="成员" width="100">
+        <el-table-column prop="userName" label="成员" width="120px">
         </el-table-column>
         <el-table-column prop="projectKey" label="项目">
         </el-table-column>
-        <el-table-column prop="hours" label="工时" width="100">
+        <el-table-column prop="hours" label="工时" width="70px">
         </el-table-column>
       </el-table>
     </el-card>
   
-    <el-dialog title="新增工时" :visible.sync="dialogFormVisible" size="tiny">
-      <el-form :model="addForm" :rules="rules" ref="addForm">
+    <el-dialog :title="{add: '新增工时', modify: '编辑工时'}[mandayForm.action]" :visible.sync="dialogFormVisible" size="tiny">
+      <el-form :model="mandayForm" :rules="rules" ref="mandayForm">
         <el-form-item label="日期" prop="date" label-width="50px">
-          <el-date-picker v-model="addForm.date" type="date" placeholder="选择日期">
+          <el-date-picker v-model="mandayForm.date" type="date" placeholder="选择日期">
           </el-date-picker>
         </el-form-item>
         <el-form-item label="项目" required label-width="50px">
           <el-col :span="11">
             <el-form-item prop="projectKey">
-              <el-select v-model="addForm.projectKey" placeholder="请选择">
+              <el-select v-model="mandayForm.projectKey" placeholder="请选择">
                 <el-option v-for="item in projects" :key="item.projectKey" :label="item.projectName" :value="item.projectKey">
                 </el-option>
               </el-select>
@@ -44,20 +51,20 @@
           <el-col :span="1">&nbsp;</el-col>
           <el-col :span="7">
             <el-form-item prop="hours">
-              <el-input v-model.number="addForm.hours" max="24" min="1" type="number">
+              <el-input v-model.number="mandayForm.hours" max="24" min="1" type="number">
                 <template slot="append">H</template>
               </el-input>
             </el-form-item>
           </el-col>
         </el-form-item>
         <el-form-item label="描述" prop="description" label-width="50px">
-          <el-input v-model="addForm.description" type="textarea" style="width: 300px;">
+          <el-input v-model="mandayForm.description" type="textarea" style="width: 300px;">
           </el-input>
         </el-form-item>
       </el-form>
       <div slot="footer" class="dialog-footer">
         <el-button @click="dialogFormVisible = false">取 消</el-button>
-        <el-button type="primary" @click="addManday()">确 定</el-button>
+        <el-button type="primary" @click="saveManday()">确 定</el-button>
       </div>
     </el-dialog>
   </div>
@@ -70,9 +77,13 @@ export default {
   name: 'manday',
   data() {
     return {
+      currentUser: '',
+      show: 'me',
       mandays: [],
       dialogFormVisible: false,
-      addForm: {
+      mandayForm: {
+        action: 'add',
+        _id: '',
         date: new Date(),
         projectKey: '',
         hours: 8,
@@ -95,14 +106,20 @@ export default {
       }
     }
   },
+  watch: {
+    show: function () {
+      this.getMandays();
+    }
+  },
   mounted() {
     this.getMandays();
     this.getProjects();
-    console.log();
+    
+    this.currentUser = localStorage.api_key;
   },
   methods: {
     getMandays() {
-      this.$http.get('/api/manday')
+      this.$http.get('/api/manday?show=' + this.show)
         .then(response => {
           this.mandays = response.data;
         });
@@ -112,28 +129,61 @@ export default {
         .then(response => {
           this.projects = response.data;
           if (this.projects.length > 0) {
-            this.addForm.projectKey = this.projects[0].projectKey;
+            this.mandayForm.projectKey = this.projects[0].projectKey;
           }
         });
     },
-    addManday() {
-      this.$refs['addForm'].validate((valid) => {
+    saveManday() {
+      this.$refs['mandayForm'].validate((valid) => {
         if (valid) {
-          this.addForm.date = moment(this.addForm.date).format('YYYY-MM-DD');
-          this.$http.post('/api/manday', this.addForm)
-            .then(response => {
-              this.dialogFormVisible = false;
-              this.$refs.addForm.resetFields();
-              this.getMandays();
-            });
+          this.mandayForm.date = moment(this.mandayForm.date).format('YYYY-MM-DD');
+          if (this.mandayForm.action == 'add') {
+            this.$http.post('/api/manday', this.mandayForm)
+              .then(response => {
+                this.dialogFormVisible = false;
+                this.$refs.mandayForm.resetFields();
+                this.getMandays();
+              });
+          }
+          else {
+            this.$http.put('/api/manday/' + this.mandayForm._id, this.mandayForm)
+              .then(response => {
+                this.dialogFormVisible = false;
+                this.$refs.mandayForm.resetFields();
+                this.getMandays();
+              });
+          }
         } else {
           return false;
         }
       });
     },
-    openAddForm() {
-      this.dialogFormVisible = true;
+    openMandayForm(row) {
       this.getProjects();
+
+      if (!!row) {
+        this.mandayForm.action = "modify";
+        this.mandayForm._id = row._id;
+        this.mandayForm.date = new Date(row.date);
+        this.mandayForm.projectKey = row.projectKey;
+        this.mandayForm.hours = row.hours;
+        this.mandayForm.description = row.description;
+      }
+      else {
+        this.mandayForm.action = "add";
+      }
+
+      this.dialogFormVisible = true;
+    },
+    deleteManday(index, row) {
+      this.$http.delete('/api/manday/' + row._id)
+        .then(response => {
+          this.$message({
+            type: 'success',
+            message: '删除成功'
+          });
+          this.getMandays();
+        });
     }
   }
 }
