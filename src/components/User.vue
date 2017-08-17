189a100c4ec59b1c1773aa69348def195b81d8a7
hueidou <hueidou163@gmail.com>
2017-08-17 17:54:58 +0800

    day commit

--- src/components/User.vue
+++ src/components/User.vue
@@ -3,6 +3,10 @@
     <el-card>
       <div slot="header" class="card-header">
         <span>成员</span>
+        <el-select v-model="value">
+          <el-option v-for="item in options" :key="item.value" :label="item.label" :value="item.value">
+          </el-option>
+        </el-select>
         <el-button type="primary" size="mini" style="float:right;margin-right: 10px;" @click="getUsers()">
           <i class="fa fa-refresh" style="width: 12px;"></i>
         </el-button>
@@ -25,7 +29,29 @@ export default {
   name: 'user',
   data() {
     return {
-      users: []
+      users: [],
+      options: [
+        {
+          value: '',
+          label: '今天'
+        },
+        {
+          value: '',
+          label: '前一天'
+        },
+        {
+          value: '',
+          label: '本周'
+        },
+        {
+          value: '',
+          label: '上周'
+        },
+        {
+          value: '',
+          label: '本月'
+        },
+      ]
     }
   },
   mounted() {
