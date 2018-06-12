ad92ad56deef81a4ff9c884178b189ade492185a
hueidou <hueidou163@gmail.com>
2018-06-12 23:48:45 +0800

    mongodb link use docker link

--- server/db/db.js
+++ server/db/db.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 mongoose.Promise = require('bluebird');
-mongoose.connect('mongodb://' + process.env.MONGODB_CONNECTION, { useMongoClient: true });
+mongoose.connect('mongodb://mongodb/wfteam', { useMongoClient: true });
 
 /**
  * 项目
