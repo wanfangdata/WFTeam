a83370ecbcaae67bc593e7d96dfb446cceaca3e0
hueidou <hueidou163@gmail.com>
2018-10-16 22:02:13 +0800

    productionSourceMap: false

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,17 @@
 module.exports = {
   outputDir: 'server/public',
+
   devServer: {
     proxy: {
       '/api': {
         target: 'http://localhost:8000'
       }
     }
+  },
+
+  productionSourceMap: false,
+
+  pwa: {
+    name: 'WFTeam'
   }
 }
